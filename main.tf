9b2eb28d652e874d1be072609d1f6e684953413d
sandeep-kengal reddy <casandeepkengal@gmail.com>
2024-02-08 12:14:12 +0000

    Simple S3 Bucket #11

--- main.tf
+++ main.tf
@@ -4,16 +4,32 @@ terraform {
       source = "hashicorp/random"
       version = "3.6.0"
     }
+      aws = {
+    source = "hashicorp/aws"
+    version = "5.35.0"
   }
 }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
 
 provider "random" {
   # Configuration options
 }
 
+# https://registry.terraform.io/providers/hashicorp/random/latest
 resource "random_string" "bucket_name" {
-  length           = 16
-  special          = false
+  lower = true
+  upper = false
+  length = 32
+  special = false
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {
